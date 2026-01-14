60323e4f4d9c4c59cdf7852c5f6af4a453178fcd
Yepod <Williamrossander@hotmail.com>
2026-01-14 10:44:53 +0100

    Disabling tests temporarily

--- src/main/java/se/jensen/william/springboot/config/SecurityConfig.java
+++ src/main/java/se/jensen/william/springboot/config/SecurityConfig.java
@@ -56,7 +56,6 @@ public class SecurityConfig {
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
 
