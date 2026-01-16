26e9f2fde3cae2cc756845d8a8b38b552f785865
Yepod <Williamrossander@hotmail.com>
2026-01-16 14:12:20 +0100

    re-activated authentication

--- src/main/java/se/jensen/william/springboot/config/SecurityConfig.java
+++ src/main/java/se/jensen/william/springboot/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui.html"
                         ).permitAll()
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
