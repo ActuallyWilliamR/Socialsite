d591ae63e828125db4ed791b814a3d7d220cd8d0
DiggyKaze <paerzae@gmail.com>
2026-01-19 23:20:58 +0100

    La till GetId för att kuna hämta id till Authcontrollern

--- src/main/java/se/jensen/william/springboot/controller/AuthController.java
+++ src/main/java/se/jensen/william/springboot/controller/AuthController.java
@@ -35,7 +35,7 @@ public class AuthController {
                 )
         );
         MyUserDetails details = (MyUserDetails) auth.getPrincipal();
-        details.getId();
+
         String token = tokenService.generateToken(auth);
 
         return ResponseEntity.ok(new LoginResponseDTO(token, details.getId()));
